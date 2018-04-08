cf5fa7700b6112c39d211e645631aaa63ccc2175
Jin <dkwk1254@naver.com>
2018-04-09 04:17:01 +0900

    ProfileActivity

--- app/src/main/java/com/project/network/ssugaeting/fragment/ProfileFragment.java
+++ app/src/main/java/com/project/network/ssugaeting/fragment/ProfileFragment.java
@@ -1,6 +1,5 @@
 package com.project.network.ssugaeting.fragment;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
